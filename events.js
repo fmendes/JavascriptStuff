c14b5ef0d34a8f1449f415d8245bb06e2d2101c2
Fernando Fernandez <fmendes@email.com>
2020-09-30 21:04:14 -0500

    Update events.js

--- events.js
+++ events.js
@@ -5,4 +5,18 @@ form.addEventListener( 'submit', (e) => {
     console.log( 'submit event triggered' ); 
 });
 
+// UL element containing several LI elements
+const ulElement = document.getElementById( 'mainList' );
 
+ulElement.addEventListener( 'click', (evt) => { 
+    evt.preventDefault(); 
+    
+    // evt.currentTarget = UL element
+    // evt.target = LI element that was clicked inside the UL element
+});
+
+// NOTE:  when something is left-clicked, the event types will be in this sequence:  mousedown, mouseup, clicked
+// right-click sequence:  mousedown
+// keystrokes sequence:  keydown, keypress, keyup
+// SHIFT, CTRL, non-character key sequence:  keydown, keyup
+// CAPS LOCK key sequence:  keydown when CAPS ON, keyup when CAPS OFF
