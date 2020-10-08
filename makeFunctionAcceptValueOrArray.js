1f639fa3fb430bf6132fc9384ad1ca6b9e8d5ef1
Fernando Fernandez <fmendes@email.com>
2020-10-08 12:42:06 -0500

    Create makeFunctionAcceptValueOrArray.js

---
+++ makeFunctionAcceptValueOrArray.js
@@ -0,0 +1,10 @@
+
+
+function refresh( wrapperNameArray ) {
+    // if a single string specified, convert it to array
+    if( typeof wrapperNameArray === 'string' ) {
+        wrapperNameArray = [ wrapperNameArray ];
+    }
+    
+    ...
+}
